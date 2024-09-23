71295c07a36cebec44d6a599debe02a9c68738aa
Matthew Lee <Matthew_Lee@avenues-ny.org>
2024-09-22 21:38:12 -0400

    P5 Sketch - Profile Picture

---
+++ sketch.js
@@ -0,0 +1,49 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(253,166,0);
+  
+  fill('white')
+  stroke('white')
+  circle(50,50,75)
+  circle(25,150,90)
+  circle(75,200,85)
+  circle(25,225,60)
+  circle(75,300,75)
+  circle(50,350,100)
+  rect(0,350,100,300)
+  circle(100,400,100)
+  circle(125,490,100)
+  circle(120,570,100)
+  circle(150,570,100)
+  circle(575,300,70)
+  rect(540,300,70,300)
+  circle(550,340,89)
+  circle(550,400,100)
+  circle(525,450,75)
+  circle(500,500,100)
+  circle(520,570,90)
+  
+  fill('red')
+  stroke('red')
+  circle(300,300,150)
+  rect(225,300,150,200)
+  rect(325,400,50,200)
+  rect(225,400,50,200)
+  
+  fill('black')
+  stroke('black')
+  circle(300,290,90)
+  rect(225,340,150,10)
+  rect(250,375,10,75)
+  rect(340,375,10,75)
+  rect(250,400,100,20)
+  rect(225,490,150,10)
+  
+  fill('blue')
+  stroke('blue')
+  rect(325,550,50,20)
+  rect(225,550,50,20)
+}
